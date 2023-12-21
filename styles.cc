1bca7e02ce27f654f66585fd5787335e4f394702
gtarecover <141908809+gtarecover@users.noreply.github.com>
2023-12-20 21:13:42 -0800

    Create styles.cc

---
+++ styles.cc
@@ -0,0 +1,19 @@
+css
+body {
+    font-family: Arial, sans-serif;
+    margin: 30px;
+}
+
+h1 {
+    text-align: center;
+    color: #333;
+}
+
+h2 {
+    color: #555;
+}
+
+p {
+    color: #777;
+}
+```
